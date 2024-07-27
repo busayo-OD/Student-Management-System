6febe291ee8a1d4b944b4725fd58c4000904cfef
Busayo Dada <toyinoluwabusayo@gmail.com>
2024-07-27 16:31:34 +0100

    openapi documentation

---
+++ src/main/java/com/school/studentmanagementsystem/config/OpenAPIConfig.java
@@ -0,0 +1,37 @@
+package com.school.studentmanagementsystem.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Busayo",
+                        email = "toyinoluwabusayo@gmail.com"
+                ),
+                description = "Student Management System",
+                title = "Student Management System",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Auth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfig {
+}
